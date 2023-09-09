c7ae55dd011f74a36e84fa5b7f2db313376cfef7
Victor Ajibade <adesojimav@gmail.com>
2023-09-09 18:49:23 +0100

    HNG-task-1

---
+++ PgCreation.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+   
+    const btn = document.getElementById("btn");
+
+   
+    const dayOfWeek = document.getElementById("DayOfWeek-now");
+    const time = document.getElementById("time-now");
+
+    const now = new Date();
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  let index = now.getDay()
+
+
+
+    btn.addEventListener("click", function () {
+       
+        if (dayOfWeek.style.display === "none") {
+            dayOfWeek.innerText = daysOfWeek[index].toString();
+            time.innerText = now;
+            dayOfWeek.style.display = "block";
+            time.style.display = "block";
+        } else {
+            dayOfWeek.style.display = "none";
+            time.style.display = "none";
+        }
+    });
+});
